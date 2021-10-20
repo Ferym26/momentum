e8369b5a9cbb3315dfd3e8245c0b14d3eca7e781
ferym26 <ferym26@gmail.com>
2021-10-20 18:57:05 +0300

    feat: add blockquots

--- src/components/Blockquote/Blockquote.vue
+++ src/components/Blockquote/Blockquote.vue
@@ -1,10 +1,34 @@
 <template lang='pug'>
-	.blockquote Blockquote
+	.blockquote
+		.blockquote__action
+			button.blockquote__reload(
+				@click='getRandomQuote()'
+			) Reload
+		.blockquote__content
+			.blockquote__title {{ quote.text }}
+			.blockquote__name {{ quote.author }}
 </template>
 
 <script>
+import blockquotes from "@/assets/data/blockquotes.js"
 export default {
 	name: 'Blockquote',
+	data() {
+		return {
+			quote: {},
+		}
+	},
+	mounted() {
+		this.getRandomQuote();
+	},
+	methods: {
+		random() {
+			return Math.floor(Math.random() * (20 - 1) + 1);
+		},
+		getRandomQuote() {
+			this.quote = blockquotes[this.random()];
+		}
+	},
 }
 </script>
 
