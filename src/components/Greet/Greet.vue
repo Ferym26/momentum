983826c22e2d26a598ed68d1c2082ec432be9ee3
ferym26 <ferym26@gmail.com>
2021-10-23 18:51:16 +0300

    fix: fix

--- src/components/Greet/Greet.vue
+++ src/components/Greet/Greet.vue
@@ -1,16 +1,18 @@
 <template lang='pug'>
 	.greet
-		.greet__title Доброе утро,
+		.greet__title {{ setTitle() + ',' }}
 		.greet__form
+			.greet__name {{ name }}
 			input.greet__input(
 				v-model='name'
 				@input="setNameToLS()"
 				type="text"
-				placeholder="[Введите имя]"
+				placeholder="[Enter name]"
 			)
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
 	name: 'Greet',
 	data() {
@@ -21,6 +23,11 @@ export default {
 	},
 	mounted() {
 		this.getNameFromLS();
+	},
+	computed: {
+		...mapGetters({
+			dayPeriod: 'getDayPeriod',
+		}),
 	},
 	methods: {
 		getNameFromLS() {
@@ -35,6 +42,24 @@ export default {
 			this.timer = window.setTimeout(() => {
 				localStorage.setItem('name', this.name);
 			}, 500);
+		},
+		setTitle() {
+			if (this.dayPeriod === 1) {
+				return 'Good night'
+			}
+			if (this.dayPeriod === 2) {
+				return 'Good morning'
+			}
+			if (this.dayPeriod === 3) {
+				return 'Good afternoon'
+			}
+			if (this.dayPeriod === 4) {
+				return 'Good evening'
+			}
+			// с 6:00 до 11:59 - Good morning / Доброе утро / Добрай раніцы
+			// с 12:00 до 17:59 - Good afternoon / Добрый день / Добры дзень
+			// с 18:00 до 23:59 - Good evening / Добрый вечер / Добры вечар
+			// с 00:00 до 5:59 - Good night / Доброй/Спокойной ночи / Дабранач
 		},
 	},
 }
