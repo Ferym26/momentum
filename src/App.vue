366010e8f92cff9068a6026c1f804a8a845df0a4
ferym26 <ferym26@gmail.com>
2021-10-23 21:04:38 +0300

    fix: readme, add checklist

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,96 @@ export default {
 		Time,
 		Greet,
 	},
+	created() {
+		this.checkList();
+	},
+	methods: {
+		checkList() {
+			const taskArr = []; // массив объектов с заданиями
+			// список заданий со значениями самооценки
+			const taskList = [
+				{
+					task: 'Часы и календарь',
+					value: 15,
+				},
+				{
+					task: 'Приветствие +10',
+					value: 10,
+				},
+				{
+					task: 'Смена фонового изображения',
+					value: 20,
+				},
+				{
+					task: 'Виджет погоды',
+					value: 15,
+				},
+				{
+					task: 'Виджет цитата дня',
+					value: 10,
+				},
+				{
+					task: 'Аудиоплеер',
+					value: 15,
+				},
+				{
+					task: 'Продвинутый аудиоплеер',
+					value: 0,
+				},
+				{
+					task: 'Перевод приложения на два языка (en/ru или en/be)',
+					value: 0,
+				},
+				{
+					task: 'Получение фонового изображения от API',
+					value: 0,
+				},
+				{
+					task: 'Настройки приложения',
+					value: 0,
+				},
+				{
+					task: '',
+					value: 0,
+				},
+				{
+					task: '',
+					value: 0,
+				},
+				{
+					task: '',
+					value: 0,
+				},
+				{
+					task: '',
+					value: 0,
+				},
+				{
+					task: '',
+					value: 0,
+				},
+			];
+			// конструктор строки задания
+			function Row (task, value) {
+				this.task = task;
+				this.value = value;
+			}
+			// наполняем массив
+			taskList.forEach((item) => {
+				taskArr.push(new Row(item.task, item.value))
+			});
+			// считаем сумму значений
+			const calcSumm = () => {
+				let summ = 0;
+				taskArr.forEach((item) => {
+					summ += item.value
+				})
+				return summ
+			};
+			// выводим в консоль таблицу списка заданий и вычислинный результат
+			console.table([...taskArr, new Row('Итого', calcSumm())]);
+		},
+	}
 }
 </script>
 
