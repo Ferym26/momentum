f30fe48555f942b93444d3e942d9588ccae8f92e
ferym26 <ferym26@gmail.com>
2021-10-24 12:14:40 +0300

    fix: move checklist to utils folder

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ import Time from "@/components/Time/Time";
 import Greet from "@/components/Greet/Greet";
 import Settings from "@/components/Settings/Settings";
 import MCursor from "@/components/MCursor/MCursor";
+import checklist from "@/utils/checklist.js"
 export default {
 	name: 'App',
 	components: {
@@ -31,80 +32,10 @@ export default {
 		Settings,
 		MCursor,
 	},
+	mixins: [checklist],
 	created() {
 		this.checkList();
 	},
-	methods: {
-		checkList() {
-			const taskArr = []; // массив объектов с заданиями
-			// список заданий со значениями самооценки
-			const taskList = [
-				{
-					task: 'Часы и календарь',
-					value: 15,
-				},
-				{
-					task: 'Приветствие +10',
-					value: 10,
-				},
-				{
-					task: 'Смена фонового изображения',
-					value: 20,
-				},
-				{
-					task: 'Виджет погоды',
-					value: 15,
-				},
-				{
-					task: 'Виджет цитата дня',
-					value: 10,
-				},
-				{
-					task: 'Аудиоплеер',
-					value: 15,
-				},
-				{
-					task: 'Продвинутый аудиоплеер',
-					value: 0,
-				},
-				{
-					task: 'Перевод приложения на два языка (en/ru или en/be)',
-					value: 0,
-				},
-				{
-					task: 'Получение фонового изображения от API',
-					value: 0,
-				},
-				{
-					task: 'Настройки приложения',
-					value: 0,
-				},
-				{
-					task: 'Дополнительный функционал на выбор: кастомный курсор',
-					value: 0,
-				},
-			];
-			// конструктор строки задания
-			function Row (task, value) {
-				this.task = task;
-				this.value = value;
-			}
-			// наполняем массив
-			taskList.forEach((item) => {
-				taskArr.push(new Row(item.task, item.value))
-			});
-			// считаем сумму значений
-			const calcSumm = () => {
-				let summ = 0;
-				taskArr.forEach((item) => {
-					summ += item.value
-				})
-				return summ
-			};
-			// выводим в консоль таблицу списка заданий и вычислинный результат
-			console.table([...taskArr, new Row('Итого', calcSumm())]);
-		},
-	}
 }
 </script>
 
