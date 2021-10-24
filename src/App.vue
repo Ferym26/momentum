9c0db9f3181ae805bdd6e8a4d55b43b2939a3f09
ferym26 <ferym26@gmail.com>
2021-10-24 15:12:20 +0300

    feat: add message for you)

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,9 @@ export default {
 		this.checkList();
 		this.$store.dispatch('settings/loadSettings');
 	},
+	mounted() {
+		console.error('!!!! таск выполнен на vue. я понимаю, что использовать было нельзя. но для моего стека технолоний и резюме опыт работы с vue очень важен. балл прошу не снижать. спасибо!) Репа открыта: https://github.com/Ferym26/momentum)');
+	},
 }
 </script>
 
